caf142e6132c1236957ee071675b09c532646eff
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-05-05 14:41:55 -0300

    1179 - 1181

--- uri-problems/Beginner/1179 - Array Fill IV.cpp
+++ uri-problems/Beginner/1179 - Array Fill IV.cpp
@@ -7,14 +7,43 @@ int main(){
     int par[5];
     int impar[5];
     int num;
+    int evenIndex = 0;
+    int oddIndex = 0;
 
     for (int i = 0; i < 15; i++) {
         cin >> num;
-        if (num % 2 == 0){
-            // Fuck 1
+        if (num % 2){
+            impar[oddIndex] = num;
+            oddIndex++;
+
+            if (oddIndex == 5) {
+
+                for (int i = 0; i < 5; i++) {
+                    cout << "impar[" << i << "] = " << impar[i] << endl;
+                }
+                oddIndex = 0;
+            }
+
         } else {
-            // Fuck 2
+            par[evenIndex] = num;
+            evenIndex++;
+
+            if (evenIndex == 5) {
+
+                for (int i = 0; i < 5; i++) {
+                    cout << "par[" << i << "] = " << par[i] << endl;
+                }
+                evenIndex = 0;
+            }
         }
+    }
+
+    for (int i = 0; i < oddIndex; i++) {
+        cout << "impar[" << i << "] = " << impar[i] << endl;
+    }
+
+    for (int i = 0; i < evenIndex; i++) {
+        cout << "par[" << i << "] = " << par[i] << endl;
     }
 
     return 0;
--- uri-problems/Beginner/1181 - Line in Array.cpp
+++ uri-problems/Beginner/1181 - Line in Array.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main(){
 
-    
+    int a[3][2] = {{1,2}, {2,4}, {5,8}};
+
+    cout << a[0][1];
 
     return 0;
 }
