724d5ce193e4c9f2411590f644871a5c536fba7a
aluno-uffs <Aluno UFFS>
2018-05-07 20:25:50 -0300

    .

---
+++ uri-problems/Origins/OBI 2013/1930 - Electrical Outlet.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int t1, t2, t3, t4;
+
+    cin >> t1 >> t2 >> t3 >> t4;
+
+    int out = (t1 + t2 + t3 + t4) - 3;
+    cout << out << endl;
+
+    return 0;
+}
