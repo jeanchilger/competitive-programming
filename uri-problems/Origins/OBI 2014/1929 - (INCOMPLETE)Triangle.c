35f598eb64d2da1263e047af1555d8347967c979
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-05-29 14:04:35 -0300

    A useful algorithm added.

---
+++ uri-problems/Origins/OBI 2014/1929 - (INCOMPLETE)Triangle.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int * bubbleSort(int arr[], int size) {
+    /*
+    ordena em ordem decrescente;
+    */
+    int temp;
+
+    for(int i = 0; i < size; i++) {
+
+        for(int j = 0; j < size; j++) {
+            if(arr[i] > arr[j]) {
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+
+int main() {
+
+    int sideList[4];
+
+    scanf(" %d %d %d %d", &sideList[0], &sideList[1], &sideList[2], &sideList[3]);
+    int* arr = bubbleSort(sideList, 4);
+
+    if (arr[0] < arr[1]+arr[2]) {
+        printf("S\n");
+
+    } else if (arr[1] < arr[2]+arr[3]) {
+        printf("S\n");
+
+    } else {
+        printf("N\n");
+    }
+
+    return 0;
+}
