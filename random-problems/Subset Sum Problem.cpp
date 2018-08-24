f6b7bae9bdc2d0b70242a82d1c4c82c62d86606f
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-08-23 23:34:52 -0300

    Easy problems removed (1)

---
+++ random-problems/Subset Sum Problem.cpp
@@ -0,0 +1,16 @@
+// from: https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+int solve () {
+
+}
+
+int main () {
+
+    return 0;
+}
--- uri-problems/Beginner/2146 - Password.py
+++
@@ -1,6 +0,0 @@
-while True:
-    try:
-        num = int(input())
-        print(num-1)
-    except:
-        break
