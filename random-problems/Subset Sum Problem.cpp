ca038c512188d3b126435b87325e4b6bcb1607e4
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-08-25 01:08:15 -0300

    .

--- random-problems/Subset Sum Problem.cpp
+++ random-problems/Subset Sum Problem.cpp
@@ -4,13 +4,29 @@
 
 using namespace std;
 
+vector<vector<int> > cache;
+vector<int> elements;
+int n;
+int sum;
 
-
-int solve () {
+int solve (int i, int j) { // i -> elemento; j -> soma a ser alcançada
 
 }
 
 int main () {
 
+    int val;
+
+    cout << "N. of elements, total sum, elements\n";
+
+    cin >> n >> sum;
+    cache.assign(n, vector<int>(sum, -1));
+
+    for (int i = 0; i < n; i++) {
+        cin >> val;
+        elements.push_back(val);
+    }
+
+    cout << solve(0, 0);
     return 0;
 }
