534c66341256b1cc1fed00bf46e11f025738af41
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-08-24 23:16:24 -0300

    Some changes.

--- uri-problems/Beginner/1564 - Brazil World Cup.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        num = int(input())
-        if not num:
-            print("vai ter copa!")
-        else:
-            print("vai ter duas!")
-    except:
-        break
